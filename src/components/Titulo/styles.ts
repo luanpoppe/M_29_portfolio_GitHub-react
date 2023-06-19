5db3fb6b75d296c4dec2da3cf4ad9f8d75453eb9
luanpoppe <118562068+luanpoppe@users.noreply.github.com>
2023-06-19 15:04:13 -0300

    novo commit

--- src/components/Titulo/styles.ts
+++ src/components/Titulo/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { Props } from "."
 
 export const Titulo = styled.h3<Props>`
-  color: 282A35;
+  color: ${(props) => props.theme.corPrincipal};
   font-size: ${(props) => (props.fontSize ? props.fontSize + "px" : "14px")};
   font-weight: bold;
   margin-bottom: 16px;
